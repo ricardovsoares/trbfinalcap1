b190f8eac89c400ab3d7f90fa84d0b5447c4c9f7
ricardovsoares <ricardovsoares@gmail.com>
2020-09-29 19:06:02 -0300

    Projeto Criado, Configuracoes Properties e H2

---
+++ backend/src/main/java/br/com/cirros/trbfinalcap1/Trbfinalcap1Application.java
@@ -0,0 +1,13 @@
+package br.com.cirros.trbfinalcap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trbfinalcap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trbfinalcap1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/cirros/trbfinalcap1/Trbfinalcap1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cirros.trbfinalcap1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trbfinalcap1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
