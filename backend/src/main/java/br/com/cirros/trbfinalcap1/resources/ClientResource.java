34060fd5230f7a15916d4dda223d3554a37ab021
ricardovsoares <ricardovsoares@gmail.com>
2020-09-29 22:21:25 -0300

    Busca Paginada de Clientes

--- backend/src/main/java/br/com/cirros/trbfinalcap1/resources/ClientResource.java
+++ backend/src/main/java/br/com/cirros/trbfinalcap1/resources/ClientResource.java
@@ -3,10 +3,14 @@ package br.com.cirros.trbfinalcap1.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cirros.trbfinalcap1.dto.ClientDTO;
@@ -20,19 +24,33 @@ public class ClientResource {
 	@Autowired
 	private ClientService service;
 	
-	@GetMapping
-	public ResponseEntity<List<Client>> findAll(){
-		//Por qustões acadêmicas, estou deixando comentado o código de teste inicial
-//		List<Client> list = new ArrayList<>();
+//	@GetMapping
+//	public ResponseEntity<List<Client>> findAll(){
+//		//Por qustões acadêmicas, estou deixando comentado o código de teste inicial
+////		List<Client> list = new ArrayList<>();
+////		
+////		list.add(new Client(1L, "Ricardo Soares", "029.215.226-40", 1000.00, Instant.now(), 2));
+////		list.add(new Client(1L, "Ricardo Soares 1", "029.215.226-40", 1000.00, Instant.now(), 2));
+////		list.add(new Client(1L, "Ricardo Soares 2", "029.215.226-40", 1000.00, Instant.now(), 2));
+////		list.add(new Client(1L, "Ricardo Soares 3", "029.215.226-40", 1000.00, Instant.now(), 2));
 //		
-//		list.add(new Client(1L, "Ricardo Soares", "029.215.226-40", 1000.00, Instant.now(), 2));
-//		list.add(new Client(1L, "Ricardo Soares 1", "029.215.226-40", 1000.00, Instant.now(), 2));
-//		list.add(new Client(1L, "Ricardo Soares 2", "029.215.226-40", 1000.00, Instant.now(), 2));
-//		list.add(new Client(1L, "Ricardo Soares 3", "029.215.226-40", 1000.00, Instant.now(), 2));
+//		List<Client> list = service.findAll();
+//		return ResponseEntity.ok().body(list);
+//	}
+	
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAll(
+				@RequestParam(value = "page", defaultValue = "0") Integer page,
+				@RequestParam(value = "linesPerPage", defaultValue = "3") Integer linesPerPage,
+				@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+				@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+
+			){
 		
-		List<Client> list = service.findAll();
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Page<ClientDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
-	}
+	}	
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
--- backend/src/main/java/br/com/cirros/trbfinalcap1/services/ClientService.java
+++ backend/src/main/java/br/com/cirros/trbfinalcap1/services/ClientService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,4 +36,10 @@ public class ClientService {
 		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Nenhum Cliente Encontrado!"));
 		return new ClientDTO(entity);
 	}
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}	
 }
